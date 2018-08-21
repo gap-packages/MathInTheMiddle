eaece2e6f04f23c25db2186527d3f1e8414e3798
Michael Torpey <mct25@st-andrews.ac.uk>
2018-08-21 14:06:40 +0100

    Comments in ToStream.gd

--- gap/ToStream.gd
+++ gap/ToStream.gd
@@ -1,4 +1,11 @@
+# Get a record from an object
 DeclareAttribute("MitM_OMRecObj", IsObject);
+
+# Get a string from a record
 DeclareGlobalFunction("MitM_StringOMRec");
+
+# Shortcut to make a simple OMS from a GAP object
 DeclareGlobalFunction("MitM_SimpleOM");
+
+# Do everything and print an object's XML
 DeclareGlobalFunction("MitM_Print");
