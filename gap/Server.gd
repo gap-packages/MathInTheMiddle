f5a4a9b4cba5f8b83a1d1e493a7d7fedf37824ae
Markus Pfeiffer <markus.pfeiffer@st-andrews.ac.uk>
2018-09-04 17:17:02 +0100

    Cleanup

--- gap/Server.gd
+++ gap/Server.gd
@@ -7,16 +7,8 @@ DeclareInfoClass("InfoMitMServer");
 DeclareInfoClass("InfoMitMTCPHandler");
 
 #!
-DeclareInfoClass("InfoMitMJsonHandler");
-
-#!
 DeclareGlobalFunction("MitM_MakeBindAddr");
 
 #!
 DeclareGlobalFunction("MitM_StartServer_TCP");
 
-#!
-DeclareGlobalFunction("MitM_JsonHandler");
-
-#!
-DeclareGlobalFunction("MitM_InterpretJsonML");
