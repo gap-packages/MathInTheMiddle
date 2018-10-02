67c5fdb86164dff570e1e2412fdf6731ade2f63e
Markus Pfeiffer <markus.pfeiffer@st-andrews.ac.uk>
2018-10-02 16:21:24 +0100

    Because strings seem to be incompatible, lets try ints, k?

--- tst/scscp/client.py
+++ tst/scscp/client.py
@@ -2,7 +2,7 @@ from openmath.openmath import *
 from scscp import SCSCPCLI
 
 c = SCSCPCLI('localhost', 26133)
-c.send(OMString("bla"))
+c.send(OMInteger(1))
 c = SCSCPCLI('localhost', 26133)
 c.heads.scscp_transient_1.MitM_Evaluate([1])
 
